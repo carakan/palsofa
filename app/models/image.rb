ae2b6d118ad043bf58f01b2bc6ed6543fcef3224
Carlos Ramos <carakan@gmail.com>
2010-05-02 23:20:31 -0300

    Added correct form

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,11 +1,10 @@
 class Image < ActiveRecord::Base
-  has_attached_file :image, :styles => { :tiny => '70x50#', :small => "210x115#" },
-    :storage => :database
+  has_attached_file(:image, :styles => { :tiny => '70x50#', :small => "210x115#" }, :storage => :database)
 
   default_scope select_without_file_columns_for(:image)
 
   validates_attachment_presence :image
-  validates_attachment_size :image, :less_than => 5.megabytes
+  validates_attachment_size :image, :less_than => 1.megabyte
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
 
   belongs_to :attachable, :polymorphic => true
